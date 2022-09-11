4d919604f6b9fde37260ddfbfa1f31e603aabf72
fred <fred.chen@live.com>
2022-09-11 07:28:39 +0000

    initial push.

---
+++ init.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+usage() {
+    printf "\nUsage: $(basename $0) <cloud_image_dir>\n\n"
+    exit 1
+}
+
+init_ubuntu() {
+    apt-get update
+    apt-get -y install python3-pip
+    apt-get install cloud-image-utils
+    pip3 install PyYaml
+}
+
+main () {
+    CLOUD_IMG_DIR=$1
+    [[ -z ${CLOUD_IMG_DIR} ]] && usage
+    [[ -d ${CLOUD_IMG_DIR} ]] || { 
+            echo "${CLOUD_IMG_DIR} does not exist! Making one..."; 
+            mkdir -p ${CLOUD_IMG_DIR}; 
+        }
+
+    source /etc/os-release && echo "OS: $NAME"
+
+    case $NAME in
+        "Ubuntu") init_ubuntu
+            ;;
+        *) echo "Unsupported OS Distribution"
+            ;;
+    esac
+}
+
+main $@
